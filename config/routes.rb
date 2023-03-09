349b13d374a539ecc1805fe78dcb1a986f1953ac
natsuki <su811sh1@gmail.com>
2023-03-09 22:09:45 +0900

    update: users#index,show_mygadgets,show_favorites、profiles#editのルーティングを追加 #11

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,24 @@
 Rails.application.routes.draw do
+  root "gadgets#top"
+
   resources :gadgets do
     collection do
       get 'top'
     end
   end
 
-  devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    passwords: 'users/passwords'
+  }
+
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
+
+  get '/users', to: 'users#index', as: 'users'
+  get '/users/mygadgets/:id', to: 'users#show_mygadgets', as: 'user_mygadgets'
+  get '/users/favorites/:id', to: 'users#show_favorites', as: 'user_favorites'
+
+  resources :profiles, only: [:edit]
 end
