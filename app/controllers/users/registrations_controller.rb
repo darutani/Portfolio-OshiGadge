98ea1adda73066c668a1b689a68a9dcca1d95463
natsuki <su811sh1@gmail.com>
2023-03-16 09:33:45 +0900

    update: 現在のパスワードの入力不要でUser情報を更新できるように修正 #23
    ・registrations_controller.rbにて、update_resourceメソッドをupdate_without_current_passwordにてオーバーライド
    ・user.rbにて、update_without_current_passwordメソッドを定義

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,9 +1,15 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   before_action :ensure_normal_user, only: %i[update destroy]
 
+  private
+
   def ensure_normal_user
     if resource.email == 'guest@example.com'
       redirect_to root_path, alert: 'ゲストユーザーの更新・削除はできません。'
     end
+  end
+
+  def update_resource(resource, params)
+    resource.update_without_current_password(params)
   end
 end
