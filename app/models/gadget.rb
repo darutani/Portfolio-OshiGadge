7e9eed35c521e72ab88ec4217d51c5ec478ca3be
natsuki <su811sh1@gmail.com>
2023-04-13 16:17:57 +0900

    update: ガジェット新規登録ページのバリデーションの設定 #49

--- app/models/gadget.rb
+++ app/models/gadget.rb
@@ -1,4 +1,9 @@
 class Gadget < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  validates :name, presence: true
+  validates :category, presence: true
+  validates :point, presence: true
+
 end
