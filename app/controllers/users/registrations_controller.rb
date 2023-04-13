d6e4aa179bbdec0e050d700efaae4c0941764342
natsuki <su811sh1@gmail.com>
2023-04-13 22:32:58 +0900

    update: プロフィール編集ページのモバイルデザイン作成 #52
    ・プロフィール編集、アカウント情報編集ページのリダイレクト先をそれぞれマイページ、アカウント情報ページになるように修正
    ・上記対応のため、プロフィール編集、アカウント情報編集ページのformに隠しフィールドを追加。隠しフィールドに基づいて、リダイレクト先を分岐する条件をuserコントローラーに追加

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,17 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   before_action :ensure_normal_user, only: %i[update destroy]
 
+  def update
+    super do |resource|
+      if resource.errors.empty?
+        case params[:form_type]
+        when 'profile_edit' then redirect_to user_mygadgets_path(current_user.id) and return
+        when 'account_edit' then redirect_to user_account_path(current_user.id) and return
+        end
+      end
+    end
+  end
+
   private
 
   def ensure_normal_user
