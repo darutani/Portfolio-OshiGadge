89ae0afafc491eb4f3614c03edc2e3be78bc8505
natsuki <su811sh1@gmail.com>
2023-04-15 16:56:29 +0900

    update: いいねしたユーザー一覧ページのアクションを追加 #65

--- app/controllers/gadgets_controller.rb
+++ app/controllers/gadgets_controller.rb
@@ -58,6 +58,10 @@ class GadgetsController < ApplicationController
     end
   end
 
+  def liked_users
+    
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_gadget
