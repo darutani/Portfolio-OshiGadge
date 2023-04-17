ee4b398a165c67d94c26bc822f3d4d8a0cafcc28
natsuki <su811sh1@gmail.com>
2023-04-17 17:46:42 +0900

    update: コメント機能のバリデーションを追加 #66

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,9 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :gadget
+
+  validates :content, presence: true, length: { minimum: 1, maximum: 400 }
+  validates :user_id, presence: true
+  validates :gadget_id, presence: true
+
 end
