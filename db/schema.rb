fbaf1ffd941e3559c43c69db7a74064835574655
natsuki <su811sh1@gmail.com>
2023-04-18 16:17:16 +0900

    add: relationshipのdb:migrateを実施 #67

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_15_085634) do
+ActiveRecord::Schema.define(version: 2023_04_18_061319) do
 
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
@@ -72,6 +72,16 @@ ActiveRecord::Schema.define(version: 2023_04_15_085634) do
     t.index ["gadget_id"], name: "index_likes_on_gadget_id"
     t.index ["user_id", "gadget_id"], name: "index_likes_on_user_id_and_gadget_id", unique: true
     t.index ["user_id"], name: "index_likes_on_user_id"
+  end
+
+  create_table "relationships", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["followed_id"], name: "index_relationships_on_followed_id"
+    t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
+    t.index ["follower_id"], name: "index_relationships_on_follower_id"
   end
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
