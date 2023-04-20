78aa8c63d881825989187c3f067a973bfd611257
natsuki <su811sh1@gmail.com>
2023-04-20 10:11:14 +0900

    add: フォロー・フォロワー一覧ページを新規作成 #67

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -23,13 +23,13 @@ class RelationshipsController < ApplicationController
 
   # フォロー一覧
   def followings
-    user = User.find(params[:user_id])
-    @users = user.followings
+    @user = User.find(params[:user_id])
+    @users = @user.followings.page(params[:page])
   end
 
   # フォロワー一覧
   def followers
-    user = User.find(params[:user_id])
-    @users = user.followers
+    @user = User.find(params[:user_id])
+    @users = @user.followers.page(params[:page])
   end
 end
