5c66fb77551786962973497366cca66b2cb68d0a
natsuki <su811sh1@gmail.com>
2023-04-30 15:18:41 +0900

    update: 不要なコメントを削除 #94

--- schema.sql
+++ schema.sql
@@ -1,27 +1,7 @@
--- MySQL dump 10.13  Distrib 8.0.32, for macos13.0 (arm64)
---
--- Host: localhost    Database: Oshigadge_App_development
--- ------------------------------------------------------
--- Server version	8.0.32
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
 --
 -- Table structure for table "active_storage_attachments"
 --
-
 DROP TABLE IF EXISTS active_storage_attachments;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE active_storage_attachments (
   id SERIAL PRIMARY KEY,
   name VARCHAR(255) NOT NULL,
@@ -32,15 +12,11 @@ CREATE TABLE active_storage_attachments (
   UNIQUE (record_type, record_id, name, blob_id),
   FOREIGN KEY (blob_id) REFERENCES active_storage_blobs (id)
 );
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table "active_storage_blobs"
 --
-
 DROP TABLE IF EXISTS active_storage_blobs;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE active_storage_blobs (
   id BIGSERIAL PRIMARY KEY,
   key VARCHAR(255) NOT NULL,
@@ -53,15 +29,11 @@ CREATE TABLE active_storage_blobs (
   created_at TIMESTAMP NOT NULL
 );
 CREATE UNIQUE INDEX index_active_storage_blobs_on_key ON active_storage_blobs (key);
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table "active_storage_variant_records"
 --
-
 DROP TABLE IF EXISTS "active_storage_variant_records";
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE active_storage_variant_records (
   id BIGSERIAL PRIMARY KEY,
   blob_id BIGINT NOT NULL,
@@ -70,30 +42,22 @@ CREATE TABLE active_storage_variant_records (
   FOREIGN KEY (blob_id) REFERENCES active_storage_blobs (id)
 );
 CREATE UNIQUE INDEX index_active_storage_variant_records_uniqueness ON active_storage_variant_records (blob_id, variation_digest);
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table "ar_internal_metadata"
 --
-
 DROP TABLE IF EXISTS ar_internal_metadata;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE ar_internal_metadata (
   key VARCHAR(255) NOT NULL PRIMARY KEY,
   value VARCHAR(255) DEFAULT NULL,
   created_at TIMESTAMP NOT NULL,
   updated_at TIMESTAMP NOT NULL
 );
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table "comments"
 --
-
 DROP TABLE IF EXISTS comments;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE comments (
   id BIGSERIAL PRIMARY KEY,
   user_id BIGINT NOT NULL,
@@ -106,15 +70,11 @@ CREATE TABLE comments (
 );
 CREATE INDEX index_comments_on_user_id ON comments (user_id);
 CREATE INDEX index_comments_on_gadget_id ON comments (gadget_id);
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table "gadgets"
 --
-
 DROP TABLE IF EXISTS gadgets;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE gadgets (
   id BIGSERIAL PRIMARY KEY,
   name VARCHAR(255) DEFAULT NULL,
@@ -129,15 +89,11 @@ CREATE TABLE gadgets (
   user_id BIGINT NOT NULL,
   FOREIGN KEY (user_id) REFERENCES users (id)
 );
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table "likes"
 --
-
 DROP TABLE IF EXISTS likes;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE likes (
   id BIGSERIAL PRIMARY KEY,
   user_id BIGINT NOT NULL,
@@ -150,15 +106,11 @@ CREATE TABLE likes (
 );
 CREATE INDEX index_likes_on_user_id ON likes (user_id);
 CREATE INDEX index_likes_on_gadget_id ON likes (gadget_id);
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table "relationships"
 --
-
 DROP TABLE IF EXISTS relationships;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE relationships (
   id BIGSERIAL PRIMARY KEY,
   follower_id INT DEFAULT NULL,
@@ -169,28 +121,20 @@ CREATE TABLE relationships (
 );
 CREATE INDEX index_relationships_on_follower_id ON relationships (follower_id);
 CREATE INDEX index_relationships_on_followed_id ON relationships (followed_id);
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table "schema_migrations"
 --
-
 DROP TABLE IF EXISTS schema_migrations;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE schema_migrations (
   version VARCHAR(255) NOT NULL,
   PRIMARY KEY (version)
 );
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table "users"
 --
-
 DROP TABLE IF EXISTS users;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE users (
   id BIGSERIAL PRIMARY KEY,
   email VARCHAR(255) NOT NULL DEFAULT '',
@@ -214,15 +158,3 @@ CREATE TABLE users (
   UNIQUE (reset_password_token),
   UNIQUE (unlock_token)
 );
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2023-04-29  8:29:26
