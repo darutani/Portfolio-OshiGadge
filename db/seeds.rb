cb3303b71052b29c34c81e81d1bb055c8cb9b325
natsuki <su811sh1@gmail.com>
2023-05-01 10:39:28 +0900

    update: 開発環境下にダミーデータを作成 #94

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,11 +2,15 @@ require 'faker'
 
 # Userのダミーデータ作成
 20.times do
+  # 改行を含む２or３行の文を作成し、変数introductionに代入
+  introduction = "#{Faker::Lorem.sentence}\n#{Faker::Lorem.sentence}"
+  introduction += "\n#{Faker::Lorem.sentence}" if rand(2) == 1
+
   user = User.create!(
     email: Faker::Internet.unique.email,
     password: 'password', # 仮のパスワードを入力
     name: Faker::Name.name,
-    introduction: Faker::Lorem.paragraph(sentence_count: 2)
+    introduction: introduction,
   )
 
   # ActiveStorageで画像を添付
@@ -19,13 +23,13 @@ end
     name: Faker::Device.model_name,
     start_date: Faker::Date.between(from: 10.years.ago, to: Date.today),
     category: Faker::Commerce.department(max: 1),
-    reason: Faker::Lorem.paragraph(sentence_count: 2),
-    point: Faker::Lorem.paragraph(sentence_count: 2),
-    usage: Faker::Lorem.paragraph(sentence_count: 2),
-    feature: Faker::Lorem.paragraph(sentence_count: 2),
+    reason: Faker::Lorem.sentences(number: 3).join("\n"),
+    point: Faker::Lorem.sentences(number: 3).join("\n"),
+    usage: Faker::Lorem.sentences(number: 3).join("\n"),
     user_id: User.all.sample.id # ランダムなユーザーIDを入力
   )
 
   # ActiveStorageで画像を添付
-  gadget.image.attach(io: File.open('app/assets/images/default_gadget_image.jpg'), filename: 'default_gadget_image.jpg', content_type: 'image/jpg')
+  gadget.image.attach(io: File.open('app/assets/images/sample_gadget_image.jpg'), filename: 'sample_gadget_image.jpg', content_type: 'image/jpg
+  ')
 end
