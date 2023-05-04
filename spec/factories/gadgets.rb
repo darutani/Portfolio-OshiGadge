ec573e3b385b38e1a3354858c2c9ba3df9142c90
natsuki <su811sh1@gmail.com>
2023-05-04 15:28:04 +0900

    update: Gadgetモデルのモデルスペックテストの追記 #100

--- spec/factories/gadgets.rb
+++ spec/factories/gadgets.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :gadget do
-    
+    user
+    name { Faker::Device.name }
+    category { Faker::Commerce.department(max: 1) }
+    point { Faker::Lorem.sentence }
   end
 end
--- spec/models/gadget_spec.rb
+++ spec/models/gadget_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Gadget, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:user) { create(:user) }
+  let(:gadget) { create(:gadget, user: user) }
+
+  describe '#liked_by?' do
+
+    context 'いいねされている時' do
+      before { create(:like, user: user, gadget: gadget) }
+
+      it 'returns true' do
+        expect(gadget.liked_by?(user)).to be true
+      end
+    end
+
+    context 'いいねされていない場合' do
+      it 'returns false' do
+        expect(gadget.liked_by?(user)).to be false
+      end
+    end
+  end
 end
