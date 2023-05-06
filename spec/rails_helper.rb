931423012ec203b08dcad47abeae2cf8a335e1c3
natsuki <su811sh1@gmail.com>
2023-05-06 09:02:35 +0900

    update: Rspecテストの設定を追加 #100
    ・rack_testを追加
    ・Deviseのメソッドをテストでも使用できるよう設定を追加

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -62,4 +62,13 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
 
   config.include FactoryBot::Syntax::Methods
+
+  # システムスペックテストのためのrack_test
+  config.before(:each, type: :system) do
+    driven_by :rack_test
+  end
+
+  # deviseのテストヘルパー
+  config.include Devise::Test::ControllerHelpers, type: :view
+  config.include Devise::Test::IntegrationHelpers, type: :system
 end
