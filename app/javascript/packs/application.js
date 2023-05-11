31d4f069262b2622ffac41aaed5120e2c9f58775
natsuki <su811sh1@gmail.com>
2023-05-11 15:31:46 +0900

    update: コメント欄が未入力の場合送信ボタンを押下できないように制限 #102
    ・comments_controller.rbにコメントのDBへの登録失敗時に不要なbuildされた@commentを削除するコードを追加
    ・Viewのform_withにて@commentを渡すためにgadgets_controller.rbのshowアクションにて新規に@comment変数を定義
    ・Viewにてコメント入力欄と送信ボタンにjavascriptにて渡すためのidを設定
    ・apllication.jsファイルにて、コメントが未入力の場合送信ボタンがdisabledになるようjavascriptの関数を定義

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -21,17 +21,73 @@ ActiveStorage.start()
 // コメント一覧ページにてガジェット詳細を開く・閉じる
 document.addEventListener('DOMContentLoaded', () => {
   const collapseButton = document.querySelector('[data-bs-toggle="collapse"]');
-  const collapseText = collapseButton.querySelector('.collapse-text');
 
-  collapseButton.addEventListener('click', () => {
-    if (collapseButton.getAttribute('aria-expanded') === 'true') {
-      console.log('開く');
-      collapseText.textContent = '▼ ガジェット詳細を閉じる';
-    } else {
-      console.log('閉じる');
-      collapseText.textContent = '▲ ガジェット詳細を見る';
-    }
-  });
+  if (collapseButton) {
+    const collapseText = collapseButton.querySelector('.collapse-text');
+
+    collapseButton.addEventListener('click', () => {
+      if (collapseButton.getAttribute('aria-expanded') === 'true') {
+        console.log('開く');
+        collapseText.textContent = '▼ ガジェット詳細を閉じる';
+      } else {
+        console.log('閉じる');
+        collapseText.textContent = '▲ ガジェット詳細を見る';
+      }
+    });
+  }
 });
 
+// コメント入力欄が未入力の場合送信ボタンを無効化
+// document.addEventListener('DOMContentLoaded', () => {
+//   const commentInput = document.getElementById('comment-input');
+//   const submitButton = document.getElementById('submit-button');
+
+//   // コメント入力欄の内容が変更されたときに呼び出される関数
+//   function onCommentInputChange() {
+//     if (commentInput.value.trim() === '') {
+//       submitButton.disabled = true;
+//     } else {
+//       submitButton.disabled = false;
+//     }
+//   }
+
+//   // コメント入力欄の初期状態に基づいて、送信ボタンの無効化/有効化を設定
+//   onCommentInputChange();
+
+//   // コメント入力欄の内容が変更されたときに、送信ボタンの無効化/有効化を更新
+//   commentInput.addEventListener('input', onCommentInputChange);
+
+//   console.log("Hello")
+// });
+
+
+// コメント入力欄が未入力の場合送信ボタンを無効化
+function onCommentInputChange() {
+  const commentInput = document.getElementById('comment-input');
+  const submitButton = document.getElementById('submit-button');
+
+  if (!commentInput || !submitButton) {
+    return;
+  }
+
+  if (commentInput.value.trim() === '') {
+    submitButton.disabled = true;
+  } else {
+    submitButton.disabled = false;
+  }
+}
+
+function initializeCommentFormValidation() {
+  const commentInput = document.getElementById('comment-input');
+
+  if (!commentInput) {
+    return;
+  }
+
+  onCommentInputChange();
+  commentInput.addEventListener('input', onCommentInputChange);
+}
+
+document.addEventListener('DOMContentLoaded', initializeCommentFormValidation);
+document.addEventListener('turbolinks:load', initializeCommentFormValidation);
 
