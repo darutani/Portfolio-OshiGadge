2dc79ca179c2c2b2c2a5828bcb7142758e9f2fbf
natsuki <su811sh1@gmail.com>
2023-05-11 15:16:58 +0900

    docs: 不要な空白を削除 #102

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   devise_for :users,
     controllers: {
       registrations: 'users/registrations',
-      passwords: 'users/passwords', 
+      passwords: 'users/passwords',
       sessions: 'users/sessions'
     }
 
